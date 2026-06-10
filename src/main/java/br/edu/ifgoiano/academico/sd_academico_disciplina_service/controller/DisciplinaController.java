36345f2104b7da3aa356e608f6e48c5d47cc5a02
LuisHRodrigues <luishenriquebr505@gmail.com>
2026-06-10 16:43:52 -0300

    fix: restaurar imports do slf4j Logger no DisciplinaController apos o merge

--- src/main/java/br/edu/ifgoiano/academico/sd_academico_disciplina_service/controller/DisciplinaController.java
+++ src/main/java/br/edu/ifgoiano/academico/sd_academico_disciplina_service/controller/DisciplinaController.java
@@ -6,6 +6,8 @@ import br.edu.ifgoiano.academico.sd_academico_disciplina_service.service.Discipl
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
